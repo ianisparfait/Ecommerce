9c699a3f9389ef52e7afe075dcf8657f799a4fc4
Ianis <ianis.parfait@marklix.com>
2022-01-31 10:31:19 +0100

    commandes dashboard

--- src/components/Commandes.js
+++ src/components/Commandes.js
@@ -2,7 +2,9 @@ import React from "react";
 import withContext from "../withContext";
 
 const Commandes = props => {
-  console.log(props)
+  const { commandes } = props.context;
+  const commandesKeys = Object.keys(commandes || {});
+  console.log(commandes, commandesKeys)
   return (
     <>
       <div className="hero is-primary">
@@ -14,17 +16,14 @@ const Commandes = props => {
       <div className="container">
         <table className="table is-bordered is-striped" style={{textAlign: "center"}}>
           <thead>
-            <tr>
-              <th>Id commande</th>
-              <th>Client</th>
-              <th>Produits</th>
-              <th>Quantités</th>
-              <th>Adresse de livraison</th>
-              <th>Montent de la commande</th>
-              <th>Transporteurs</th>
-              <th>État de la commande</th>
-              <th>Actions</th>
-            </tr>
+            {commandes.map(c => {
+              return (
+                <tr>
+                  <td>{c.transaction_id}</td>
+                  <td><button className="button is-primary">Afficher le détail</button></td>
+                </tr>
+              )
+            })}
           </thead>
           <tbody>
             <tr>
